144bbd5a2267b8fbd900f39e900eb2f97192704e
JayMar921 <jayharronabejar@gmail.com>
2023-09-18 20:57:40 +0800

    Dashboard Partial Update - Profile card is now shown - Logout Feature Added - Dashboard route changed to '/' [temporarily]

---
+++ PSITS_Web.WebAssembly/Pages/Dashboard.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using PSITS_Web.Common;
+using PSITS_Web.WebAssembly.Services;
+
+namespace PSITS_Web.WebAssembly.Pages
+{
+	public partial class Dashboard
+	{
+		[Inject]
+		private AuthenticationApiService AuthenticationApiService { get; set; }
+
+		[Inject]
+		private NavigationManager NavigationManager { get; set; }
+
+		public User User { get; set; } = new User();
+
+		protected async override Task OnInitializedAsync()
+		{
+			// grab the user data from API call
+			User _userData = await AuthenticationApiService.GetCurrentUser();
+
+			// check if user is not null, otherwise, return to login page
+			if (_userData == null)
+			{
+				NavigationManager.NavigateTo("/login");
+			}
+			else
+			{
+				User = _userData;
+			}
+			
+		}
+	}
+}
