0e70c96729e79d04fdf79fe5174ce52d0cbb2321
JayMar921 <jayharronabejar@gmail.com>
2023-09-21 13:06:41 +0800

    Added OfficeLog Extensions Utility

---
+++ PSITS_Web.WebAssembly/Utils/OfficeLogsExtension.cs
@@ -0,0 +1,25 @@
+﻿using PSITS_Web.Common;
+
+namespace PSITS_Web.WebAssembly.Utils
+{
+    public static class OfficeLogsExtension
+    {
+        public static OfficeLogs GetLogs(this OfficeLogs officeLogs, DateTime date)
+        {
+            List<OfficeLog> logList = new List<OfficeLog>();
+
+            foreach(OfficeLog log in officeLogs.officeLogs)
+            {
+                if (log.loginTime.SameDay(date))
+                    logList.Add(log);
+            }
+
+            return new OfficeLogs() { officeLogs = logList };
+        }
+
+        private static bool SameDay(this DateTime date, DateTime compareDate)
+        {
+            return date.Day == compareDate.Day && date.Month == compareDate.Month && date.Year == compareDate.Year;
+        }
+    }
+}
