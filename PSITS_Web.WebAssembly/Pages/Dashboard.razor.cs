34c2f14486a7a960ce36b9b2f233ac4189cdff33
JayMar921 <jayharronabejar@gmail.com>
2023-09-21 13:07:50 +0800

    Updated Dashboard - Added Icons to Calendar Logs - Next/Previous month is now functional

--- PSITS_Web.WebAssembly/Pages/Dashboard.razor.cs
+++ PSITS_Web.WebAssembly/Pages/Dashboard.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using PSITS_Web.Common;
 using PSITS_Web.WebAssembly.Services;
+using PSITS_Web.WebAssembly.Utils;
 
 namespace PSITS_Web.WebAssembly.Pages
 {
@@ -12,9 +13,19 @@ namespace PSITS_Web.WebAssembly.Pages
 		[Inject]
 		private NavigationManager NavigationManager { get; set; }
 
-		public User User { get; set; } = new User();
+        [Inject]
+        public IOfficeLogDataService OfficeLogDataService { get; set; }
 
-		protected async override Task OnInitializedAsync()
+        private OfficeLogs OfficeLogs = new();
+        private DateTime Date;
+        public User User { get; set; } = new User();
+
+        protected override void OnInitialized()
+        {
+            Date = DateTime.Now;
+        }
+
+        protected async override Task OnInitializedAsync()
 		{
 			// grab the user data from API call
 			User _userData = await AuthenticationApiService.GetCurrentUser();
@@ -28,7 +39,15 @@ namespace PSITS_Web.WebAssembly.Pages
 			{
 				User = _userData;
 			}
-			
-		}
-	}
+            // grab the data
+            await LoadOfficeLogs();
+        }
+
+        private async Task LoadOfficeLogs()
+        {
+            OfficeLogs = await OfficeLogDataService.GetAllOfficeLogs("", new DateTime(Date.Year, Date.Month, 1), new DateTime(Date.Year, Date.Month, CalendarUtils.GetNumberOfDaysInMonth(Date.Year, Date.Month)));
+            Console.WriteLine($"Loaded Office Logs for [Year: {Date.Year}, Month: {Date.Month}]");
+            StateHasChanged();
+        }
+    }
 }
